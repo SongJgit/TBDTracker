9bb0312c445e1787bb7db7bf70cb51925b14cd17
_SongJ <songj9507@outlook.com>
2025-12-14 22:20:48 +0800

    update scripts

---
+++ scripts/track_from_fold_dance_sort_lakf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DATASET_CFG="dancetrack-val"
+TRACKER="sort"
+KALMAN_FORMAT="sortknet"
+MIN_AREA=150
+CONF_THRESH_LOW=0.1
+CONF_THRESH=0.5
+TRACK_BUFFER=30
+INIT_TRACK_THRESH=0.6
+MATCH_THRESH=0.7
+DETECTED_FOLDER="./results_detected/dance/val"
+EVAL_YAML="DanceTrack"
+
+SAVE_FOLDER="fromdet-SIKNet" 
+MOTION_MODEL_PATH="./weights/siknet_dance.ckpt"
+
+# 执行跟踪命令
+python track.py \
+    --dataset "$DATASET_CFG" \
+    --tracker "$TRACKER" \
+    --kalman_format "$KALMAN_FORMAT" \
+    --motion_model_path "$MOTION_MODEL_PATH" \
+    --min_area "$MIN_AREA" \
+    --conf_thresh_low "$CONF_THRESH_LOW" \
+    --conf_thresh "$CONF_THRESH" \
+    --track_buffer "$TRACK_BUFFER" \
+    --init_thresh "$INIT_TRACK_THRESH" \
+    --match_thresh "$MATCH_THRESH" \
+    --eval_yaml "$EVAL_YAML" \
+    --save_folder "$SAVE_FOLDER" \
+    file \
+    --detected_folder "$DETECTED_FOLDER"
---
+++ scripts/track_from_fold_snomt_byte_lakf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATASET_CFG="soccernet"
+TRACKER="bytetrack"
+KALMAN_FORMAT="byte"
+MIN_AREA=150
+CONF_THRESH_LOW=0.1
+CONF_THRESH=0.6
+TRACK_BUFFER=30
+INIT_TRACK_THRESH=0.6
+DETECTED_FOLDER="/home/feng/songj_workspace/MOT_Datasets/SNMOT/test"
+EVAL_YAML="SNMOT"
+
+SAVE_FOLDER="fromdet-SIKNet" 
+MOTION_MODEL_PATH="./weights/siknet_soccernet.ckpt"
+
+# 执行跟踪命令
+python track.py \
+    --dataset "$DATASET_CFG" \
+    --tracker "$TRACKER" \
+    --kalman_format "$KALMAN_FORMAT" \
+    --motion_model_path "$MOTION_MODEL_PATH" \
+    --min_area "$MIN_AREA" \
+    --conf_thresh_low "$CONF_THRESH_LOW" \
+    --conf_thresh "$CONF_THRESH" \
+    --track_buffer "$TRACK_BUFFER" \
+    --init_thresh "$INIT_TRACK_THRESH" \
+    --eval_yaml "$EVAL_YAML" \
+    --save_folder "$SAVE_FOLDER" \
+    file \
+    --detected_folder "$DETECTED_FOLDER"
